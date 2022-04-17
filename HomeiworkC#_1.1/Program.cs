d15dff4438f38c8e50c06fe956cdd6111e78972b
Artemiy <azazellorart@gmail.com>
2022-04-17 22:00:52 +0500

    HomeworkC#_1.1

--- HomeiworkC#_1.1/Program.cs
+++ HomeiworkC#_1.1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA > numberB)
+{
+    Console.WriteLine("Наибольшее число: " + numberA);
+    Console.WriteLine("Наименьшее число: " + numberB);
+}
+else
+{
+    Console.WriteLine("Наибольшее число: " + numberB);
+    Console.WriteLine("Наименьшее число: " + numberA);
+
+}
