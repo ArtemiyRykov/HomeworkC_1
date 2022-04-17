693030a6b27a17ac9c835f1ae4e3910fdfc9163d
Artemiy <azazellorart@gmail.com>
2022-04-18 00:48:47 +0500

    HomeworkC#_1.4

---
+++ HomeworkC#_1.4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 2;
+while(a <= N)
+{
+    Console.Write(a);
+    a = a + 2;
+    if(a <= N)
+    {
+        Console.Write(", ");
+    }
+}
